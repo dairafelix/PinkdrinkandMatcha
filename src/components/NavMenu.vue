2d929370b3e071f963def6164262bd48fa38e8f4
Daira Felix <dairafelix@icloud.com>
2022-11-15 21:55:20 -0600

    Customized Navbar

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,17 +1,18 @@
-<script setup>
-import { ref } from 'vue'
-const brand = ref('Pinkdrink and Matcha')
-</script>
+<script setup></script>
 
 <template>
+  <div class="saleBanner">
+    <h3>Now offering International Shipping! | Free US shipping on orders over $35 with code FREESHIPPING</h3>
+  </div>
   <nav>
     <div class="wrapper">
-      <div class="brand">
-        <span class="brand-title">{{ brand }}</span>
-      </div>
+      <img width="300" src="logo.png" />
       <div class="menu">
-        <a href="#" class="menu-item">Products</a>
-        <a href="#" class="menu-item">Shopping Cart</a>
+        <a href="#" class="menu-item">Home</a>
+        <a href="#" class="menu-item">Shop</a>
+        <a href="#" class="menu-item">About</a>
+        <!--Display the number of items on cart-->
+        <a href="#" class="menu-item">🛒(0)</a>
         <a href="#" class="menu-login">Logout</a>
       </div>
     </div>
@@ -19,22 +20,23 @@ const brand = ref('Pinkdrink and Matcha')
 </template>
 
 <style scoped lang="postcss">
+.saleBanner {
+  @apply flex h-12 items-center justify-center bg-[#b5c940] text-white;
+}
 nav {
-  @apply flex h-20 bg-rose-300 text-lime-400;
+  @apply flex h-40;
+  img {
+    @apply flex items-center justify-items-center;
+  }
   .wrapper {
-    @apply container mx-auto flex w-full items-center justify-between;
-    .brand {
-      &-title {
-        @apply text-2xl font-bold text-yellow-800;
-      }
-    }
+    @apply mx-auto flex w-full items-center justify-between px-5;
     .menu {
       @apply flex gap-2;
       &-item {
-        @apply rounded-md px-4 py-2 hover:bg-yellow-500 hover:text-slate-900;
+        @apply rounded-md px-4 py-2 text-slate-400 hover:text-slate-600;
       }
       &-login {
-        @apply rounded-md bg-red-500 px-4 py-2 text-red-100 hover:bg-red-700;
+        @apply rounded-md bg-rose-300 px-4 py-2 text-red-100 hover:bg-red-500;
       }
     }
   }
