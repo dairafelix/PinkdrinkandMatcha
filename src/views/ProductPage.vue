f88c62ed778fa46a2ede0d4c0259e3e5616c9c7f
Daira Felix <dairafelix@icloud.com>
2022-11-20 19:41:54 -0600

    Created basic Template for Products Page

---
+++ src/views/ProductPage.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="divider pl-5">
+    <h1>PRODUCTS</h1>
+    <hr class="container mx-auto" />
+  </div>
+  <div class="grid grid-cols-4">
+    <div class="p-2">
+      <!--Card 1-->
+      <div class="overflow-hidden rounded shadow-lg">
+        <img class="w-full" src="/product.jpg" alt="Product" />
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold">Title</div>
+          <p class="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis
+            eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700"
+            >Add to Cart</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="p-2">
+      <!--Card 2-->
+      <div class="overflow-hidden rounded shadow-lg">
+        <img class="w-full" src="/product.jpg" alt="Product" />
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold">Title</div>
+          <p class="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis
+            eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700"
+            >Add to Cart</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="p-2">
+      <!--Card 3-->
+      <div class="overflow-hidden rounded shadow-lg">
+        <img class="w-full" src="/product.jpg" alt="Product" />
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold">Title</div>
+          <p class="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis
+            eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700"
+            >Add to Cart</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="p-4">
+      <!--Card 4-->
+      <div class="overflow-hidden rounded shadow-lg">
+        <img class="w-full" src="/product.jpg" alt="Product" />
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold">Title</div>
+          <p class="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis
+            eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700"
+            >Add to Cart</span
+          >
+        </div>
+      </div>
+    </div>
+
+    <!--SECOND ROW-->
+    <div class="p-4">
+      <!--Card 1-->
+      <div class="overflow-hidden rounded shadow-lg">
+        <img class="w-full" src="/product.jpg" alt="Product" />
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold">Title</div>
+          <p class="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis
+            eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700"
+            >Add to Cart</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="p-4">
+      <!--Card 2-->
+      <div class="overflow-hidden rounded shadow-lg">
+        <img class="w-full" src="/product.jpg" alt="Product" />
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold">Title</div>
+          <p class="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis
+            eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700"
+            >Add to Cart</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="p-4">
+      <!--Card 3-->
+      <div class="overflow-hidden rounded shadow-lg">
+        <img class="w-full" src="/product.jpg" alt="Product" />
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold">Title</div>
+          <p class="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis
+            eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700"
+            >Add to Cart</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="p-4">
+      <!--Card 4-->
+      <div class="overflow-hidden rounded shadow-lg">
+        <img class="w-full" src="/product.jpg" alt="Product" />
+        <div class="px-6 py-4">
+          <div class="mb-2 text-xl font-bold">Title</div>
+          <p class="text-base text-gray-700">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis
+            eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 pt-4 pb-2">
+          <span class="mr-2 mb-2 inline-block rounded-full bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700"
+            >Add to Cart</span
+          >
+        </div>
+      </div>
+    </div>
+
+    <!-- grid div ends-->
+  </div>
+
+  <!--Footer -->
+  <div class="footer">
+    <h1 class="font-bold">ABOUT</h1>
+    <a href="#" class="footer-item">About</a>
+    <a href="#" class="footer-item">FAQ</a>
+    <a href="#" class="footer-item">EMAIL</a>
+    <a href="#" class="footer-item">SOCIAL MEDIA ICONS</a>
+    <h1 class="footer-center">© PinkdrinkandMatcha 2022</h1>
+  </div>
+</template>
+
+<style scoped lang="postcss">
+.item {
+  @apply rounded-md bg-slate-400 p-4;
+  &-details {
+    @apply items-center justify-center;
+  }
+}
+.divider {
+  @apply flex h-20 items-center justify-center text-2xl font-bold text-rose-300;
+}
+.footer {
+  @apply mx-auto w-full items-center justify-between bg-rose-300 pt-5 pl-5 text-white;
+  &-item {
+    @apply flex w-full items-center py-2;
+  }
+  &-center {
+    @apply flex h-12 items-center justify-center;
+  }
+}
+</style>
